e1ff743e45d338ff3953e4471e3889c9811a6791
jc4f <lequan12122002@gmail.com>
2025-05-27 14:39:18 +0700

    authen

--- src/login-button.tsx
+++ src/login-button.tsx
@@ -7,11 +7,22 @@ export default function AuthButtons() {
   const [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
-    userManager.getUser().then((u) => setUser(u));
+    userManager.getUser().then((u) => {
+      setUser(u);
+      if (!u) {
+        userManager.signinRedirect();
+      }
+    });
     // lắng nghe sự kiện login/logout
     userManager.events.addUserLoaded((u) => setUser(u));
     userManager.events.addUserUnloaded(() => setUser(null));
   }, []);
+
+  //   useEffect(() => {
+  //     if (!user) {
+  //       userManager.signinRedirect();
+  //     }
+  //   }, []);
 
   const login = () => {
     userManager.signinRedirect();
